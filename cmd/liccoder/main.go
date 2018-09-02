1d9bbc5fc8653a3739bb79c4d8ce34a708b83b20
Alan Bunjevac <alan.bunjevac@exobyte.org>
2018-09-02 14:16:59 +0200

    License de/coder command line tool

---
+++ cmd/liccoder/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	input  string
+	output string
+	key    string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "liccoder",
+	Short: "License coder and decoder.",
+}
+
+var encodeCmd = &cobra.Command{
+	Use:   "encode",
+	Short: "Encode license file",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := encode(input, output, key); err != nil {
+			fmt.Println(err)
+
+			os.Exit(1)
+		}
+	},
+}
+
+var decodeCmd = &cobra.Command{
+	Use:   "decode",
+	Short: "Decode license file",
+	Args:  cobra.NoArgs,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := decode(input, output, key); err != nil {
+			fmt.Println(err)
+
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	encodeCmd.Flags().StringVarP(&input, "in", "i", "", "input file")
+	encodeCmd.Flags().StringVarP(&output, "out", "o", "", "output file")
+	encodeCmd.Flags().StringVarP(&key, "key-file", "k", "", "private key file")
+
+	decodeCmd.Flags().StringVarP(&input, "in", "i", "", "input file")
+	decodeCmd.Flags().StringVarP(&output, "out", "o", "", "output file")
+	decodeCmd.Flags().StringVarP(&key, "key-file", "k", "", "public key file")
+
+	if err := encodeCmd.MarkFlagRequired("key-file"); err != nil {
+		panic(err)
+	}
+
+	if err := decodeCmd.MarkFlagRequired("key-file"); err != nil {
+		panic(err)
+	}
+
+	rootCmd.AddCommand(encodeCmd, decodeCmd)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
